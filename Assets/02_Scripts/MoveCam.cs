bd3137da57ce08cc70a9667b8c7b4f4b1a457dea
MikoHeine <Miko.Heine@games-academy.de>
2025-06-04 14:13:13 +0200

    Added MoveCamScript

---
+++ Assets/02_Scripts/MoveCam.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MoveCam : MonoBehaviour
+{
+    public GameObject camera;
+    public float moveSpeed;
+    void Update()
+    {
+        ControllingCam();
+    }
+
+    public void ControllingCam()
+    {
+        Vector3 move = new Vector3();
+        
+        if (Input.GetKey(KeyCode.W)) move.y += 1;
+        if (Input.GetKey(KeyCode.S)) move.y -= 1;
+
+        camera.transform.position += move * (moveSpeed * Time.deltaTime);
+    }
+}
