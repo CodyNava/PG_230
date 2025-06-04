f50df9a214d163fbd18a316c6bfc8679e7ffda70
Grorhall <167302118+Grorhall@users.noreply.github.com>
2025-06-04 15:22:57 +0200

    [*] added pickaxe sound

--- Assets/02_Scripts/GetDickedDown.cs
+++ Assets/02_Scripts/GetDickedDown.cs
@@ -4,10 +4,22 @@ public class GetDickedDown : MonoBehaviour
 {
     private int i = 0;
     public ChangeText randomizer;
+    public AudioSource mineSound;
 
     private void IGotDickedDown()
     {
         transform.position += Vector3.down;
+        if (mineSound.isPlaying == true)
+        {
+            mineSound.Stop();
+        }
+
+        if (mineSound.isPlaying == false)
+        {
+            mineSound.pitch = Random.Range(0.95f, 1.05f);
+            mineSound.Play();
+        }
+
         if (i == Random.Range(3, 6) || i > 5)
         {
             i = 0;
