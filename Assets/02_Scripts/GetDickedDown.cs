edd742a8d90d65d20bc3f882def3ce50ae3eb3e0
Grorhall <167302118+Grorhall@users.noreply.github.com>
2025-06-04 15:37:13 +0200

    [*] now a global click amount

--- Assets/02_Scripts/GetDickedDown.cs
+++ Assets/02_Scripts/GetDickedDown.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class GetDickedDown : MonoBehaviour
 {
-    private int i = 0;
-    public ChangeText randomizer;
     public AudioSource mineSound;
 
     private void IGotDickedDown()
@@ -18,18 +16,6 @@ public class GetDickedDown : MonoBehaviour
         {
             mineSound.pitch = Random.Range(0.95f, 1.05f);
             mineSound.Play();
-        }
-
-        if (i == Random.Range(3, 6) || i > 5)
-        {
-            i = 0;
-            randomizer.UpdateText();
-            Debug.Log("Updated Text");
-        }
-        else
-        {
-            i++;
-            Debug.Log("Count up: " + i);
         }
     }
 }
