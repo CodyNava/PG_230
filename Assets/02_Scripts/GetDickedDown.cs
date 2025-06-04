5998fe9272e981a85464dec739b82f9f9f070933
Jan-Lukas Dehne <kingdomeheart16@gmail.com>
2025-06-04 14:21:48 +0200

    Quoterandomizer on digging

--- Assets/02_Scripts/GetDickedDown.cs
+++ Assets/02_Scripts/GetDickedDown.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class GetDickedDown : MonoBehaviour
 {
+    public ChangeText randomizer;
     private void IGotDickedDown()
     {
         transform.position += Vector3.down;
+        randomizer.UpdateText();
     }
 }
